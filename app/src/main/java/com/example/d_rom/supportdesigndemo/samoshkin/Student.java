7e679dac51a5878ca7004a4c2eb468ebfa76c4d3
R-Tem <samoshkin-@ukr.net>
2015-09-02 18:57:22 +0300

    Student class in /samoshkin package created

---
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/Student.java
@@ -0,0 +1,31 @@
+package com.example.d_rom.supportdesigndemo.samoshkin;
+
+/**
+ * Created by R-Tem on 02.09.2015.
+ */
+public class Student {
+
+    private String mName;
+    private String mAge;
+
+    public Student(String _name, String _age) {
+        this.mName = _name;
+        this.mAge = _age;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getAge() {
+        return mAge;
+    }
+
+    public void setAge(String mAge) {
+        this.mAge = mAge;
+    }
+}
