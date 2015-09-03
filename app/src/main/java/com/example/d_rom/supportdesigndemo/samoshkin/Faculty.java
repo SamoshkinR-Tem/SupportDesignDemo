9f4464665ecb194d6fb2d04c19d9b4ca109a7763
R-Tem <samoshkin-@ukr.net>
2015-09-03 14:41:20 +0300

    Faculty filled

--- app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/Faculty.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/Faculty.java
@@ -1,7 +1,33 @@
 package com.example.d_rom.supportdesigndemo.samoshkin;
 
+import java.util.ArrayList;
+
 /**
  * Created by R-Tem on 03.09.2015.
  */
 public class Faculty {
+
+    private String mFacTitle;
+    private ArrayList<Student> mStudents;
+
+    public Faculty(String _FacTitle, ArrayList<Student> _Students) {
+        this.mFacTitle = _FacTitle;
+        this.mStudents = _Students;
+    }
+
+    public String getFacTitle() {
+        return mFacTitle;
+    }
+
+    public void setFacTitle(String mFacTitle) {
+        this.mFacTitle = mFacTitle;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return mStudents;
+    }
+
+    public void setStudents(ArrayList<Student> mStudents) {
+        this.mStudents = mStudents;
+    }
 }
