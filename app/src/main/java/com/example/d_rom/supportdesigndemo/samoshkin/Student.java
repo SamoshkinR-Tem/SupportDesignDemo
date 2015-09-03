b22473a5efc04efb14b8f4b5169faf0cc3b082ce
R-Tem <samoshkin-@ukr.net>
2015-09-03 15:29:25 +0300

    Class Student upgraded (list of lectures added)

--- app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/Student.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/Student.java
@@ -1,5 +1,7 @@
 package com.example.d_rom.supportdesigndemo.samoshkin;
 
+import java.util.ArrayList;
+
 /**
  * Created by R-Tem on 03.09.2015.
  */
@@ -7,6 +9,7 @@ public class Student {
 
     private String mName;
     private String mAge;
+    private ArrayList<Lecture> mLectures;
 
     public Student(String _name, String _age) {
         this.mName = _name;
@@ -27,5 +30,13 @@ public class Student {
 
     public void setAge(String mAge) {
         this.mAge = mAge;
+    }
+
+    public ArrayList<Lecture> getLectures() {
+        return mLectures;
+    }
+
+    public void setLectures(ArrayList<Lecture> mLectures) {
+        this.mLectures = mLectures;
     }
 }
