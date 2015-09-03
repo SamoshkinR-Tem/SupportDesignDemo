0e93afde1a31a99bef41b1bacc88eeab445dbeb5
R-Tem <samoshkin-@ukr.net>
2015-09-03 10:23:13 +0300

    class University created

---
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/University.java
@@ -0,0 +1,33 @@
+package com.example.d_rom.supportdesigndemo.samoshkin;
+
+import java.util.ArrayList;
+
+/**
+ * Created by R-Tem on 02.09.2015.
+ */
+public class University {
+
+    private String mTitle;
+    private ArrayList<Student> mStudents;
+
+    public University(String _Title, ArrayList<Student> _Students) {
+        this.mTitle = _Title;
+        this.mStudents = _Students;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return mStudents;
+    }
+
+    public void setStudents(ArrayList<Student> mStudents) {
+        this.mStudents = mStudents;
+    }
+}
