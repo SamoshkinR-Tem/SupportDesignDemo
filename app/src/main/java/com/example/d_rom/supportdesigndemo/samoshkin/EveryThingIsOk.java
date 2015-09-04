2621d1bdf4b5770ac98bde61d18b9dbce80dfdde
R-Tem <samoshkin-@ukr.net>
2015-09-04 10:00:11 +0300

    Class EveryThingIsOk modified

--- app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/EveryThingIsOk.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/samoshkin/EveryThingIsOk.java
@@ -4,4 +4,10 @@ package com.example.d_rom.supportdesigndemo.samoshkin;
  * Created by R-Tem on 03.09.2015.
  */
 public class EveryThingIsOk {
+
+    private String mSlogan;
+
+    public EveryThingIsOk(String _Slogan) {
+        this.mSlogan = _Slogan;
+    }
 }
